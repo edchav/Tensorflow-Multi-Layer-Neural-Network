1ed7a31097e3f75776edae713f830c780002291c
Edgar Chavez <61804977+edchav@users.noreply.github.com>
2024-02-26 16:19:32 -0600

    Update Tensorflow Multi Layer Neural Network.py

--- Tensorflow Multi Layer Neural Network.py
+++ Tensorflow Multi Layer Neural Network.py
@@ -1,8 +1,3 @@
-# Chavez, Edgar
-# 1002_091_846
-# 2023_10_15
-# Assignment_02_01
-
 import numpy as np
 import tensorflow as tf
 
@@ -34,7 +29,6 @@ def split_data(X_train, Y_train, validation_split):
     # Remove validation sets from training data
     X_train_split = np.concatenate([X_train[:start], X_train[end:]], axis=0)
     Y_train_split = np.concatenate([Y_train[:start], Y_train[end:]], axis=0)  
-
     
     return X_train_split, Y_train_split, X_valid, Y_valid
 
@@ -175,54 +169,6 @@ def multi_layer_nn_tensorflow(X_train,Y_train,layers,activations,alpha,batch_siz
         validation_loss = compute_loss(Y_valid, Y_pred_valid, loss)  
         error.append(validation_loss.numpy())
     
-
     validation = forward_propagation(X_valid, weights, activations)[-1].numpy()   
 
     return weights, error, validation
-
-    # This function creates and trains a multi-layer neural Network
-    # X_train: numpy array of input for training [nof_train_samples,input_dimensions]
-    # Y_train: numpy array of desired outputs for training samples [nof_train_samples,output_dimensions]
-    # layers: Either a list of integers or alist of numpy weight matrices.
-    # If layers is a list of integers then it represents number of nodes in each layer. In this case
-    # the weight matrices should be initialized by random numbers.
-    # If the layers is given as a list of weight matrices, then the given matrices should be used and NO random
-    # initialization is needed.
-    # activations: list of case-insensitive activations strings corresponding to each layer. The possible activations
-    # are, "linear", "sigmoid", "relu".
-    # alpha: learning rate
-    # epochs: number of epochs for training.
-    # loss: is a case-insensitive string determining the loss function. The possible inputs are: "svm" , "mse",
-    # "cross_entropy". for cross entropy use the tf.nn.softmax_cross_entropy_with_logits().
-    # validation_split: a two-element list specifying the normalized start and end point to
-    # extract validation set. Use floor in case of non integers.
-    # seed: random number generator seed for initializing the weights.
-    # return: This function should return a list containing 3 elements:
-        # The first element of the return list should be a list of weight matrices.
-        # Each element of the list should be a 2-d numpy array which corresponds to the weight matrix of the
-        # corresponding layer (Biases should be included in each weight matrix in the first row).
-
-        # The second element should be a one dimensional list of numbers
-        # representing the error after each epoch. Each error should
-        # be calculated by using the validation set while the network is frozen.
-        # Frozen means that the weights should not be adjusted while calculating the error.
-
-        # The third element should be a two-dimensional numpy array [nof_validation_samples,output_dimensions]
-        # representing the actual output of the network when validation set is used as input.
-
-    # Notes:
-    # The data set in this assignment is the transpose of the data set in assignment_01. i.e., each row represents
-    # one data sample.
-    # The weights in this assignment are the transpose of the weights in assignment_01.
-    # Each output weights in this assignment is the transpose of the output weights in assignment_01
-    # DO NOT use any other package other than tensorflow and numpy
-    # Bias should be included in the weight matrix in the first row.
-    # Use steepest descent for adjusting the weights
-    # Use minibatch to calculate error and adjusting the weights
-    # Reseed the random number generator when initializing weights for each layer.
-    # Use numpy for weight to initialize weights. Do not use tensorflow weight initialization.
-    # Do not use any random method from tensorflow
-    # Do not shuffle data
-    # i.e., Initialize the weights for each layer by:
-    # np.random.seed(seed)
-    # np.random.randn()
